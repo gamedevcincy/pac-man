2b5b930962a6a98fd15aeb96b9d0b96a3ed5a0c9
WillCPP <WillCPP@users.noreply.github.com>
2019-03-16 19:54:37 -0400

    Update Pellets

---
+++ Assets/Scripts/PlayerScoreTracker.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScoreTracker : MonoBehaviour
+{
+    public int score;
+
+    void Start()
+    {
+        score = 0;
+    }
+
+    void AddScore(int points)
+    {
+        score += points;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        ScoreObject scoreObject = col.GetComponent<ScoreObject>();
+        if (scoreObject != null)
+        {
+            AddScore(scoreObject.points);
+        }
+    }
+}
