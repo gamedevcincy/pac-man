8dcbcd0d1c13b631444dff22dc811ed5664c0262
WillCPP <WillCPP@users.noreply.github.com>
2019-03-25 20:11:15 -0400

    Add Constants.cs

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants
+{
+    public const string PLAYER = "Player";
+    public const string PELLET = "Pellet";
+    public const string POWER_PELLET = "PowerPellet";
+    public const string FRUIT = "FRUIT";
+    public const string GHOST = "Ghost";
+
+}
