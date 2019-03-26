47bb2b3379c4822f2ee00d09eb697654527dc5f7
WillCPP <WillCPP@users.noreply.github.com>
2019-03-25 20:11:38 -0400

    Update GhostController.cs

--- Assets/Scripts/GhostController.cs
+++ Assets/Scripts/GhostController.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 
 public class GhostController : MonoBehaviour
 {
+    public bool inactive;
+    public float timeLeftInactive;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timeLeftInactive = 0;
+        //inactive = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!this.gameObject.activeSelf)
+        {
+            timeLeftInactive -= Time.deltaTime;
+            if (timeLeftInactive < 0)
+            {
+                //inactive = false;
+                this.gameObject.SetActive(true);
+            }
+        }
     }
 
     void OnTriggerEnter(Collider col)
@@ -24,6 +36,8 @@ public class GhostController : MonoBehaviour
             if (playerController != null && playerController.consumedPowerPellet)
             {
                 this.gameObject.SetActive(false);
+                //inactive = true;
+                timeLeftInactive = 25;
             }
         }
     }
